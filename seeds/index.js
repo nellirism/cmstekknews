9ef4d9774b88ac469af65f7151602c046de7bf4d
Cerebreo <nellirism@outlook.com>
2021-06-26 13:27:40 -0700

    fixed database connection error by removing the colon and replacing with equal sign on the env file

--- seeds/index.js
+++ seeds/index.js
@@ -3,8 +3,6 @@ const seedPosts = require('./post-seeds');
 const seedUsers = require('./user-seeds');
 const seedComments = require('./comment-seeds');
 
-const sequelize = require('../config/connection');
-
 const seedAll = async() => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
